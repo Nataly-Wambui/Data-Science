b76b42796798cbd77ddf0ccb66c14a2f0eda00bb
Nataly-Wambui <wambuinatalygakenye@gmail.com>
2024-04-02 20:50:33 +0300

    Add stack file

---
+++ stack.py
@@ -0,0 +1,25 @@
+# Creating a stack
+names = []
+
+# Adding items to a stack
+names.append("Nataly")
+names.append("Wambui")
+names.append("Gakenye")
+print(names)
+
+#Removing items from a stack
+names.pop()
+print(names)
+
+# Getting size of a stack
+length = len(names)
+print(length)
+
+if len(names) == 0:
+    print("The stack is empty")
+else:
+    print(len(names))
+
+
+
+
