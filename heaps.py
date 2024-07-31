f71e738279b1be3353ae9abb9a1323091a49fa6a
Nataly-Wambui <wambuinatalygakenye@gmail.com>
2024-07-31 21:45:24 +0300

    Adds interview questions for heaps

--- heaps.py
+++ heaps.py
@@ -54,4 +54,85 @@ merged = []
 
 
 
+##How do you find the smallest k elements in an array using a heap?
+def smallest_k_elements(arr, k):
+    return heapq.nsmallest(k, arr)
+
+print(smallest_k_elements([2,5,6,7,2,1,8,9], 3))
+
+##How do you implement a priority queue using a heap?
+class PriorityQueue:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, item, priority):
+        return heapq.heappush(self.heap, (priority, item))
+
+    def pop(self):
+        return heapq.heappop(self.heap)[1]
+
+    def peek(self):
+        return self.heap[0][1] if self.heap else None
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
+
+##How do you find the kth smallest element in a matrix sorted row-wise and column-wise?
+def kth_smallest(matrix, k):
+    n = len(matrix)
+    min_heap = []
+    for r in range(min(n, k)):  
+        heapq.heappush(min_heap, (matrix[r][0], r, 0))
+
+    number = 0
+    for _ in range(k):
+        number, r, c = heapq.heappop(min_heap)
+        if c + 1 < len(matrix[0]):
+            heapq.heappush(min_heap, (matrix[r][c + 1], r, c + 1))
+            
+    return number
+print(kth_smallest([[1, 5, 9],[10, 11, 13],[12, 13, 15]], 8))
+
+
+## How do you find the maximum sum of a sliding window of size k using a heap?
+def max_sliding_window(nums, k):
+    if not nums:
+        return []
+
+    max_heap = []
+    result = []
+    for i in range(len(nums)):
+        heapq.heappush(max_heap, (-nums[i], i))
+        if i >= k - 1:
+            while max_heap[0][1] <= i - k:
+                heapq.heappop(max_heap)
+            result.append(-max_heap[0][0])  
+
+    return result
+print(max_sliding_window([1, 3, -1, -3, 5, 3, 6, 7], 3))
+
+## How do you find the median of a stream of numbers using heaps?
+class MedianFinder:
+    def __init__(self):
+        self.max_heap = []  
+        self.min_heap = []  
+
+    def add_num(self, num):
+        heapq.heappush(self.max_heap, -num)
+        if (self.max_heap and self.min_heap and 
+            (-self.max_heap[0] > self.min_heap[0])):
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+        elif len(self.min_heap) > len(self.max_heap):
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+
+    def find_median(self):
+        if len(self.max_heap) > len(self.min_heap):
+            return -self.max_heap[0]
+        return (-self.max_heap[0] + self.min_heap[0]) / 2
+
+
 
